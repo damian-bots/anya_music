e2fa50b14fbbcf770bf7c71a4b2fb1e273286026
damian-bots <tosu0791@gmail.com>
2024-12-13 04:07:49 +0530

    Update settings.py

--- jiosaavn/config/settings.py
+++ jiosaavn/config/settings.py
@@ -2,7 +2,7 @@ from os import getenv
 
 API_ID = 24620300
 API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
-BOT_TOKEN = "6662446698:AAE-xH3Uff3-b2fkhYIYdtTVnnMmILeIXL0"
+BOT_TOKEN = "7810188596:AAF-7JouiEvT9atoQA7NPooqqkDb0UfbT3I"
 BOT_COMMANDS = (
     ("start", "Initialize the bot and check its status"),
     ("settings", "Configure and manage bot settings"),
@@ -10,6 +10,6 @@ BOT_COMMANDS = (
     ("about", "Learn more about the bot and its features"),
 )
 
-DATABASE_URL = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URL = "mongodb+srv://tusar:tusar1@cluster0.guw5d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 HOST = getenv("HOST", "0.0.0.0")
 PORT = int(getenv("PORT", "0"))
