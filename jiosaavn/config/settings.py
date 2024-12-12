5d5b50a9bfdb0110f2459fddd5420b1f6bd276f3
damian-bots <tosu0791@gmail.com>
2024-12-13 03:23:04 +0530

    Add files via upload

---
+++ jiosaavn/config/settings.py
@@ -0,0 +1,15 @@
+from os import getenv
+
+API_ID = 24620300
+API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
+BOT_TOKEN = "6662446698:AAE-xH3Uff3-b2fkhYIYdtTVnnMmILeIXL0"
+BOT_COMMANDS = (
+    ("start", "Initialize the bot and check its status"),
+    ("settings", "Configure and manage bot settings"),
+    ("help", "Get information on how to use the bot"),
+    ("about", "Learn more about the bot and its features"),
+)
+
+DATABASE_URL = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+HOST = getenv("HOST", "0.0.0.0")
+PORT = int(getenv("PORT", "0"))
